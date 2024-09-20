9eeab12356bf847aa8c9e7575a21cf29f07a7855
Дмитрий Абабков <dmitry@MacBook-Air-Dmitrij.local>
2024-09-21 02:08:23 +0500

    fix bugs

--- src/Components/Converter.tsx
+++ src/Components/Converter.tsx
@@ -5,26 +5,26 @@ function Converter() {
     const blockOutput = React.useRef<HTMLDivElement>(null);
 
     const handler:React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        setState(e.target.value)
-        if (state.length < 6) {
-            return state
-        } else {
-            checkOutput();
+        const output:HTMLDivElement | null = blockOutput.current;
+        const newValue = e.target.value;
+        setState(newValue)
+        if (e.target.value.length == 7 && e.target.value[0] === '#') {
+            checkOutput(newValue);
+        } else if (e.target.value[0] !== '#' || e.target.value.length > 7) {
+            if(output) output.textContent = `#Ошибка`;
+            setStateColor(`#4287f5`);
         }
     }
 
-    const checkOutput = ():void => {
-        const output:any = blockOutput.current;
-        if(state[0] == '#' && state.length == 6) {
-            const hex :string = state;
+    const checkOutput = (value:string):void => {
+        const output: HTMLDivElement | null = blockOutput.current;
+        if(value[0] === '#' && value.length === 7) {
+            const hex :string = value;
             const r :number = parseInt(hex.slice(1, 3), 16);
             const g :number = parseInt(hex.slice(3, 5), 16);
             const b :number = parseInt(hex.slice(5, 7), 16);
-            output.textContent = `rgb: (${r}, ${g}, ${b})`;
+           if(output) output.textContent = `rgb: (${r}, ${g}, ${b})`;
             setStateColor(`rgb(${r}, ${g}, ${b})`);
-        } else {
-            output.textContent = `#Ошибка`;
-            setStateColor(`#4287f5`);
         }
 
     }
